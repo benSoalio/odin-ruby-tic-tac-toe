903c0826943d43d0ab25ded751604d7331769062
bsoalio <bsoalio@gmail.com>
2022-07-17 23:31:10 +0000

    Finish game class

--- script.rb
+++ script.rb
@@ -11,59 +11,98 @@ end
 
 # Game Class
 class Game
-
+  WINNING_COMBO = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [4, 5, 6], [2, 5, 8], [3, 5, 7], [7, 8, 9], [3, 6, 9]]
   
+  attr_accessor :is_winner, :play_count
+
   def initialize
 
     # Take the players name and sign
     puts "Enter your name player 1: "
     name = gets.chomp
-
-    puts "Enter your sign player 1:"
-    sign = gets.chomp 
+    sign = "O" 
 
     @player_one = Player.new(name, sign)
 
     puts "Enter your name player 2: "
     name = gets.chomp
-
-    puts "Enter your sign player 2:"
-    sign = gets.chomp 
+    sign = "X" 
 
     @player_two = Player.new(name, sign)
 
     # Variable to keep track ot player turn 
     @turn = true
+
+    # Variable to keep track of a winner
+    @is_winner = false
+
+    # Variable to keep track number of plays
+    @play_count = 0
+
+
   end
-  
+
   def play
+
+    # Update the player turn 
     @current_player = @turn ? @player_one : @player_two
-  
-    # To keep track of the position already occupied
-    @positions = @player_one.positions.concat(@player_two.positions)
-    puts "positions #{@positions}"
-    @current_position = 0
-    puts " conditions include #{@positions.include?(@current_position)}"
 
-    # Repeat until the position of the player is from 1 to 9 and not already used
-    until @current_position > 0 && @current_position < 10
+    # Keep track of th position played
+    @position_played = 0
+
+    # Repeat until the position played is within 1 to 9
+    until @position_played.positive? && @position_played < 10
+      # Ask the player to choose a position 
+      puts "Choose your position between 1 and 9"
       puts "#{@current_player.name} choose your position: "
-      @current_position = gets.chomp.to_i
-      puts "player positions array #{@current_player.positions}"
-    end
+      @position_played = gets.chomp.to_i
 
-    @current_player.positions.push(@current_position)
-    puts " current position #{@current_position}"
-    # Check if there is a winner 
+      # Check if the posotion played is free
+      if @player_one.positions.include?(@position_played)
+        puts "#{@player_one.name} already played in this position"
+        # Reset position played
+        @position_played = 0
+      elsif @player_two.positions.include?(@position_played)
+        puts "#{@player_two.name} already played in this position"
+        # Reset position played
+        @position_played = 0
+      else
+        # Update the player positions array
+        @current_player.positions.push(@position_played) unless @position_played.zero? || @position_played == 10
+      end
+    end
+    puts "#{@player_two.positions} player two position"
+    puts "#{@player_one.positions} player one position"
+    
+    # Update is_winner 
+    winner?(@current_player.name, @current_player.positions)
+    
+    # Change the player turn
     @turn = !@turn
+  end 
+
+  def winner?(name, positions)
+    positions.combination(3) do |combo|
+      combo.sort!
+      if WINNING_COMBO.include?(combo)
+        @is_winner = true
+        puts "#{name} wins the game"
+        break
+      end
+    end
   end
-  
-  
 end
 
 def main
   game = Game.new
-  game.play
+  until game.is_winner 
+    game.play 
+    game.play_count += 1
+    if game.play_count == 9
+      puts "It is a tie game"
+      break
+    end
+  end
 end
 
 main
@@ -140,3 +179,35 @@ main
 #   puts result
 # end
 
+# def play
+
+#     # is_winner = 0
+
+#     # To keep track of the position already occupied
+#     @positions = []
+#     @current_position = 0
+
+#     # until is_winner > 9
+
+#       @current_player = @turn ? @player_one : @player_two
+    
+      
+#       # puts "positions #{@positions}"
+      
+#       puts " conditions include #{@positions.include?(@current_position)}"
+  
+#       # Repeat until the position of the player is from 1 to 9 and not already used
+#       until @current_position > 0 && @current_position < 10
+#         puts "#{@current_player.name} choose your position: "
+#         @current_position = gets.chomp.to_i
+#         @current_player.positions.push(@current_position) unless @positions.include?(@current_position)
+#         @positions.push(@current_position) unless @positions.include?(@current_position)
+#         puts "player positions array #{@current_player.positions}"
+#       end
+      
+#       puts "positions #{@positions}"
+#       # Check if there is a winner 
+#       @turn = !@turn
+#       # is_winner += 1
+#     # end
+#   end
